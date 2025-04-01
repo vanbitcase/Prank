1634a3377531144c5ad5d49123fb70425af6d607
Vansh Rastogi <vanshrastogi212@gmail.com>
2025-04-01 12:04:11 +0530

    added .vecel.app in allowed host

--- ghibligen/settings.py
+++ ghibligen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hcwi5s#cd)n740vbk8ymfm94d72t)(ujr=v0u^3zdmsmm1n!d6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
